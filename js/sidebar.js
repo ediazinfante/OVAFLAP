ae67e66397beed44e7522644531ba36b58e969f5
ediazinfante <ediazinfante@hotmail.com>
2017-08-22 00:59:45 -0500

    inicial

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,6 @@
 		function w3_open() {
 		  document.getElementById("main").style.marginLeft = "25%";
-		  document.getElementById("mySidebar").style.width = "50%";
+		  document.getElementById("mySidebar").style.width = "25%";
 		  document.getElementById("mySidebar").style.display = "block";
 		  document.getElementById("openNav").style.display = 'none';
 		}
@@ -9,3 +9,15 @@
 		  document.getElementById("mySidebar").style.display = "none";
 		  document.getElementById("openNav").style.display = "inline-block";
 		}
+		
+function myAccFunc(p1) {
+    var x = document.getElementById(p1);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-green";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className = 
+        x.previousElementSibling.className.replace(" w3-green", "");
+    }
+}
