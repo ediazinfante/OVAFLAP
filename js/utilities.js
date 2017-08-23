7ba406931f4674be3f31c9652f596171acd16167
ediazinfante <ediazinfante@hotmail.com>
2017-08-23 12:46:02 -0500

    hasta clasificacion

--- js/utilities.js
+++ js/utilities.js
@@ -16,35 +16,6 @@ function topFunction() {
     document.documentElement.scrollTop = 0; // For IE and Firefox
 } 
 
-/*----------- Modal Image --------*/
-/*
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-img = document.getElementById('myImg2');
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-    modal.style.display = "none";
-}*/
-
 /*---------------- LightBox ------------*/
 function openModal() {
   document.getElementById('myModal').style.display = "block";
