253b9e3567f36dcd14d4ec90bebd33472bfd2c65
ediazinfante-wrk <ediazinfante@hotmail.com>
2017-08-25 18:08:12 -0500

    Release 1

--- js/controller.js
+++ js/controller.js
@@ -7,10 +7,10 @@ var stage2=new Object;
 var questionLock=false;
 var numberOfQuestions;
 var score=0;
-		 
-		
+ 
+
  		$.getJSON('quiz.json', function(data) {
-		
+
 		for(i=0;i<data.quizlist.length;i++){ 
 			questionBank[i]=new Array;
 			questionBank[i][0]=data.quizlist[i].question;
@@ -20,17 +20,16 @@ var score=0;
 			questionBank[i][4]=data.quizlist[i].option4;
 			questionBank[i][5]=data.quizlist[i].type;
 			questionBank[i][6]=data.quizlist[i].photo;
+			questionBank[i][7]=data.quizlist[i].justify;
+
 		}
 		 numberOfQuestions=questionBank.length; 
-		
+
 		 
 		displayQuestion();
 		})//gtjson
  
  
-
-
-
 function displayQuestion(){
  var rnd=0;
  var q1;
@@ -42,6 +41,7 @@ function displayQuestion(){
 
 ty=questionBank[questionNumber][5];
 ph=questionBank[questionNumber][6];
+ju=questionBank[questionNumber][7];
 
 if(ty==0){
 //multiple options
@@ -69,21 +69,25 @@ if(rnd==1){q1=questionBank[questionNumber][1];q2=questionBank[questionNumber][2]
 if(rnd==2){q2=questionBank[questionNumber][1];q3=questionBank[questionNumber][2];q4=questionBank[questionNumber][3];q1=questionBank[questionNumber][4];}
 if(rnd==3){q3=questionBank[questionNumber][1];q4=questionBank[questionNumber][3];q1=questionBank[questionNumber][2];q2=questionBank[questionNumber][4];}
 
-$(stage).append('<div class="questionText">'+questionBank[questionNumber][0]+'</div><div class="questionPhoto"><img src="'+ph+'"></div><div id="1" class="option">'+q1+'</div><div id="2" class="option">'+q2+'</div><div id="3" class="option">'+q3+'</div><div id="4" class="option">'+q4+'</div>');
+$(stage).append('<div class="questionText">'+questionBank[questionNumber][0]+'</div><div class="questionPhoto"><p class="w3-center"><img class="w3-center" style="width:400px" src="'+ph+'"></p></div><div id="1" class="option">'+q1+'</div><div id="2" class="option">'+q2+'</div><div id="3" class="option">'+q3+'</div><div id="4" class="option">'+q4+'</div>');
 }
 
  $('.option').click(function(){
   if(questionLock==false){questionLock=true;	
   //correct answer
   if(this.id==rnd){
-   $(stage).append('<div class="feedback1">CORRECTO</div><audio autoplay><source src="audios/correcto.mp3" type="audio/mpeg"></audio>');
+   $(stage).append('<div class="feedback1">CORRECTO</div><div class="w3-row w3-text-white w3-marginTop"><br>'+ju+'</div><audio autoplay><source src="audios/correcto.mp3" type="audio/mpeg"></audio>');
    score++;
    }
   //wrong answer	
   if(this.id!=rnd){
-   $(stage).append('<div class="feedback2">FALLASTE</div><audio autoplay><source src="audios/fallaste.mp3" type="audio/mpeg"></audio>');
+   $(stage).append('<div class="feedback2">FALLASTE</div><div class="w3-row w3-text-white w3-marginTop"><br>'+ju+'</div><audio autoplay><source src="audios/fallaste.mp3" type="audio/mpeg"></audio>');
   }
-  setTimeout(function(){changeQuestion()},1000);
+  if(ju!=" "){
+	setTimeout(function(){changeQuestion()},10000);
+  }else{
+    setTimeout(function(){changeQuestion()},1000);
+  }
  }})
 }//display question
 
